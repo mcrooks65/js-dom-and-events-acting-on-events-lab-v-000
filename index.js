4bc3fd04e822723fc50c20417d317b6813c7231c
Matthew Crooks <mfcrooks@gmail.com>
2018-09-14 21:48:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function retrieveEmployeeInformation(){
 function addNewElementAsLi(){
   let employeeName = retrieveEmployeeInformation()
 document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)}
+
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+    resetInput()
+  }
+}
